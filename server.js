0ec7277bc1668b3bdad1815e713730cd5624356a
Rebecca Brietzke <rnbrietzke@outlook.com>
2022-09-25 10:28:33 -0500

    change port number

--- server.js
+++ server.js
@@ -1,7 +1,7 @@
 /** Start server for Lunchly. */
 
-const app = require("./app");
+const app = require('./app');
 
-app.listen(3000, function() {
-  console.log("listening on 3000");
+app.listen(5050, function () {
+  console.log('listening on 5050');
 });
